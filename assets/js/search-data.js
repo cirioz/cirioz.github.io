9af4ce8a8ac4d078850bdc39bcb86ac17a312348
cirioz <56224188+cirioz@users.noreply.github.com>
2026-05-17 20:32:16 +0000

    Deploying to gh-pages from @ cirioz/cirioz.github.io@f7f485de824cf9e5864fecc6fced95abaf10d7df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Full Stack QA Engineer — Python, Pytest, Playwright, Selenium. ISTQB certified.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
