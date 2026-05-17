a8327b535a46e9f5da8e70d057e3c6d3d18574c1
cirioz <56224188+cirioz@users.noreply.github.com>
2026-05-17 19:39:19 +0000

    Deploying to gh-pages from @ cirioz/cirioz.github.io@fd31424c230b920a683023a049781f6c87b882f2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-test-drawing",
+        },{id: "post-test-2",
+        
+          title: "Test 2",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/test-2/";
+          
+        },
+      },{id: "post-test-drawing",
         
           title: "Test Drawing",
         
